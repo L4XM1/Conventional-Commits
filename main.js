ef0a0a455806bbb5f4b710f6d517badf174740b6
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-02-08 23:08:01 +0100

    fix(toggle): fix toggle first list item content
    Fix main.js to toggle visibility of list item content. Error in paragraph
    text.
    Small change to style of button and add border around content output box.
    Add index paragraphs to add content.

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 const output = document.querySelector(".changing-content");
 const listItem = document.querySelectorAll(".list-item");
-
-for (const item of listItem) {
-  item.addEventListener("click", function() {
-    fetch("text.json")
-      .then(response => response.json())
-      .then(text => {
-        output.textContent = text[this.textContent];
-      });
-  });
-}
-
 const button = document.querySelector(".btn");
 const entireList = document.querySelector("ul");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   if (entireList.style.display === "none") {
     entireList.style.display = "flex";
     button.textContent = "Sakrij";
-    if (listItem.length > 0) {
-      fetch("text.json")
-        .then(response => response.json())
-        .then(text => {
-          output.textContent = text[listItems[0].textContent];
-        });
+
+    for (const item of listItem) {
+      item.addEventListener("click", function () {
+        fetch("text.json")
+          .then((response) => response.json())
+          .then((text) => {
+            output.textContent = text[this.textContent];
+          });
+      });
     }
   } else {
     entireList.style.display = "none";
@@ -31,4 +23,3 @@ button.addEventListener("click", function() {
     output.textContent = "";
   }
 });
-
